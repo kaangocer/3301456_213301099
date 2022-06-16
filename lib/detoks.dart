32e314a6232621d4275ad7140a38bb4a021e2cb3
kaangocer <kaangoceer@gmail.com>
2022-06-16 13:29:24 +0300

    dart ffi kaldırıldı

--- lib/detoks.dart
+++ lib/detoks.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+
 
 import 'package:flutter/material.dart';
 import 'package:healthylifecoach/detokspaketleri/3g%C3%BCnl%C3%BCkDetoksPaketi.dart';
@@ -33,119 +33,113 @@ class DetoksPaketleri extends StatelessWidget {
 class detoksIcerik extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Column(
-        children: [
-          Container(
-            height: 45,
-            width: 200,
-            color: Colors.pink,
-            child: Center(
-              child: Text(
-                "DETOKS PAKETLERİ",
-                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
-              ),
+    return Column(
+      children: [
+        Container(
+          height: 45,
+          width: 200,
+          color: Colors.pink,
+          child: Center(
+            child: Text(
+              "DETOKS PAKETLERİ",
+              style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
             ),
           ),
-          SizedBox(
-            height: 8,
-          ),
-          Row(
-            children: [
-              ExpandedWidget(
-                jpgDosyasi: "juiceDetoksSeti",
-                jpgAdi: "Juice Detoks Seti",
-                fiyatBilgisi: "160 TL",
-                onPress: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => juice()));
-                },
-              ),
-              ExpandedWidget(
-                jpgDosyasi: "magicPudingDetoks",
-                jpgAdi: "Magic Puding Detoks",
-                fiyatBilgisi: "200 TL",
-                onPress: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => magicPuding()));
-                },
-              )
-            ],
-          ),
-          Row(
-            children: [
-              ExpandedWidget(
-                jpgDosyasi: "magnoliaPudingDetoks",
-                jpgAdi: "Magnolia Puding Detoks",
-                fiyatBilgisi: "200 TL",
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => MagnoliaPuding()));
-                },
-              ),
-              ExpandedWidget(
-                jpgDosyasi: "WildBerryPudingDetoks",
-                jpgAdi: "Wild Berry Puding Detoks",
-                fiyatBilgisi: "200 TL",
-                onPress: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => WildBerry()));
-                },
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              ExpandedWidget(
-                jpgDosyasi: "3GünlükDetoksPaketi",
-                jpgAdi: "3 Günlük Detoks Paketi",
-                fiyatBilgisi: "495 TL",
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => UcGunlukDetoks()));
-                },
-              ),
-              ExpandedWidget(
-                jpgDosyasi: "3GünlükVejetaryenDetoksPaketi",
-                jpgAdi: "3 Günlük Vejetaryen Detoks Paketi",
-                fiyatBilgisi: "480 TL",
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => UcGunlukVejetaryen()));
-                },
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              ExpandedWidget(
-                jpgDosyasi: "3GünlükVeganDetoksPaketi",
-                jpgAdi: "3 Günlük Vegan Detoks Paketi",
-                fiyatBilgisi: "500 TL",
-                onPress: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => UcGunlukVegan()));
-                },
-              ),
-              ExpandedWidget(
-                jpgDosyasi: "5GünlükDetoksPaketi",
-                jpgAdi: "5 Günlük Detoks Paketi",
-                fiyatBilgisi: "700 TL",
-                onPress: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => BesGunlukDetoks()));
-                },
-              ),
-            ],
-          ),
-        ],
-      );
+        ),
+        SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: [
+            ExpandedWidget(
+              jpgDosyasi: "juiceDetoksSeti",
+              jpgAdi: "Juice Detoks Seti",
+              fiyatBilgisi: "160 TL",
+              onPress: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => juice()));
+              },
+            ),
+            ExpandedWidget(
+              jpgDosyasi: "magicPudingDetoks",
+              jpgAdi: "Magic Puding Detoks",
+              fiyatBilgisi: "200 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => magicPuding()));
+              },
+            )
+          ],
+        ),
+        Row(
+          children: [
+            ExpandedWidget(
+              jpgDosyasi: "magnoliaPudingDetoks",
+              jpgAdi: "Magnolia Puding Detoks",
+              fiyatBilgisi: "200 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MagnoliaPuding()));
+              },
+            ),
+            ExpandedWidget(
+              jpgDosyasi: "WildBerryPudingDetoks",
+              jpgAdi: "Wild Berry Puding Detoks",
+              fiyatBilgisi: "200 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => WildBerry()));
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            ExpandedWidget(
+              jpgDosyasi: "3GünlükDetoksPaketi",
+              jpgAdi: "3 Günlük Detoks Paketi",
+              fiyatBilgisi: "495 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => UcGunlukDetoks()));
+              },
+            ),
+            ExpandedWidget(
+              jpgDosyasi: "3GünlükVejetaryenDetoksPaketi",
+              jpgAdi: "3 Günlük Vejetaryen Detoks Paketi",
+              fiyatBilgisi: "480 TL",
+              onPress: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => UcGunlukVejetaryen()));
+              },
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            ExpandedWidget(
+              jpgDosyasi: "3GünlükVeganDetoksPaketi",
+              jpgAdi: "3 Günlük Vegan Detoks Paketi",
+              fiyatBilgisi: "500 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => UcGunlukVegan()));
+              },
+            ),
+            ExpandedWidget(
+              jpgDosyasi: "5GünlükDetoksPaketi",
+              jpgAdi: "5 Günlük Detoks Paketi",
+              fiyatBilgisi: "700 TL",
+              onPress: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => BesGunlukDetoks()));
+              },
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
